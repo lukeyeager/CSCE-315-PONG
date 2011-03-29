408f49498d0422899b4ae3c888d94265928edb15
unknown <lyeager@.cs.tamu.edu>
2011-03-29 18:39:38 -0500

    Added XNA project while keeping silverlight project for historical value

---
+++ PongMobileSilverlight/Play.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PongMobile
+{
+    public partial class Play : PhoneApplicationPage
+    {
+        public Play()
+        {
+            InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(Default_Loaded);
+        }
+        private void Default_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (NavigationContext.QueryString.Count > 0)
+            {
+                try
+                {
+                    Uri temp = new Uri(NavigationContext.QueryString.Values.First(), UriKind.RelativeOrAbsolute);
+                    GameTypeText.Text = temp.ToString();
+                }
+                catch (Exception ex)
+                {
+                    //handle the exception;
+                }
+            }
+        }
+    }
+}
