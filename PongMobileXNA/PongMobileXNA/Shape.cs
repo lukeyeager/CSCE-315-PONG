1b324c7344dc66ac4b08febe083f694bc5bf5faf
Luke Yeager <luke.yeager@gmail.com>
2011-03-31 17:55:36 -0500

    Added shape class and moved content into separate folders. Haven't gotten to touch detecting yet

---
+++ PongMobileXNA/PongMobileXNA/Shape.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PongScreenManager;
+
+namespace PongMobileXNA
+{
+    class Shape
+    {
+        //TODO: Ryan, have some fun
+    }
+}
